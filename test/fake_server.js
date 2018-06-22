afd51b4b38eeaca15ccb8e699485feddf149faf8
CaviarChen <yzj@vlabpro.com>
2018-06-22 23:31:12 +1000

    add fake server for testing

---
+++ test/fake_server.js
@@ -0,0 +1,115 @@
+const http = require('http');
+const url = require('url');
+
+const key = 'testkey';
+
+
+http.createServer(function (req, res) {
+    let urlParts = url.parse(req.url);
+    console.log(req.url);
+
+    switch(urlParts.pathname) {
+        case "/":
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.write('OK');
+            res.end();
+            break;
+
+        case "/slave/v1/init":
+            init(req, res);
+            break;
+        case "/slave/v1/fetchTask":
+            fetchTask(req, res);
+            break;
+        case "/slave/v1/submitTask":
+            submitTask(req, res);
+            break;
+
+        default:
+            res.writeHead(404);
+            res.end();
+            break;
+    }
+}).listen(8088);
+
+console.log("Server running at http://127.0.0.1:8088/");
+
+function responseJson(res, obj) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write(JSON.stringify(obj));
+    res.end();
+}
+
+
+function init(req, res) {
+    let query = url.parse(req.url, true).query;
+    let obj = {};
+    if (query['key'] == key) {
+        obj['code'] = 1;
+        obj['msg'] = 'OK';
+    } else {
+        obj['code'] = -1;
+        obj['msg'] = 'Wrong key.';
+    }
+    responseJson(res, obj);
+}
+
+function fetchTask(req, res) {
+    let query = url.parse(req.url, true).query;
+    let obj = {};
+
+    if (query['key'] != key) {
+        obj['code'] = -1;
+        obj['msg'] = 'Wrong key.';
+        responseJson(res, obj);
+        return;
+    }
+
+
+    let task = {
+        "url": "https://store.steampowered.com/stats/Steam-Game-and-Player-Statistics?l=english",
+        "timeout": 20000,
+        "fullPage": false,
+        "clip": {
+            "x": 0,
+            "width": 500,
+            "height": 1024,
+            "y": 1024
+        },
+        "viewport": {
+            "width": 500,
+            "isMobile": false
+        },
+        "output": {
+            "type": "jpg",
+            "progressive": true,
+            "quality": 80
+        }
+    };
+
+    obj['code'] = 1;
+    obj['msg'] = 'OK';
+    obj['taskid'] = 1001;
+    obj['task'] = task;
+
+    responseJson(res, obj);
+}
+
+function submitTask(req, res) {
+    let query = url.parse(req.url, true).query;
+    let obj = {};
+
+    if (query['key'] != key) {
+        obj['code'] = -1;
+        obj['msg'] = 'Wrong key.';
+        responseJson(res, obj);
+        return;
+    }
+
+    console.log(query);
+
+    obj['code'] = 1;
+    obj['msg'] = 'OK';
+
+    responseJson(res, obj);
+}
