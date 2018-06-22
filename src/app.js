3da73c434cdc2d8540a0a7e143061a2082e55944
CaviarChen <yzj@vlabpro.com>
2018-06-22 15:21:21 +1000

    add task parameters

--- src/app.js
+++ src/app.js
@@ -8,7 +8,28 @@ function main() {
 
 // ------
 function test() {
-    taskHandler.runTask();
+    let task = {
+        "url": "https://store.steampowered.com/stats/Steam-Game-and-Player-Statistics?l=english",
+        "timeout": 20000,
+        "fullPage": false,
+        "clip": {
+            "x": 0,
+            "width": 500,
+            "height": 1024,
+            "y": 1024
+        },
+        "viewport": {
+            "width": 500,
+            "isMobile": false
+        },
+        "output": {
+            "type": "jpg",
+            "progressive": true,
+            "quality": 80
+        }
+    };
+
+    taskHandler.runTask(task);
 }
 // ------
 
--- src/task-handler.js
+++ src/task-handler.js
@@ -1,14 +1,30 @@
 const fs = require('fs')
 const puppeteer = require('puppeteer');
+const sharp = require('sharp');
 
-function runTask() {
+function runTask(task) {
     (async () => {
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
-        await page.goto('https://www.google.com');
-        const buffer = await page.screenshot();
 
-        fs.writeFile('test.png', buffer, 'binary', function(err){
+        let viewport = task['viewport'];
+        if (viewport['height'] === undefined) viewport['height'] = 1024;
+
+        await page.setViewport(viewport);
+        await page.goto(task['url'], {timeout: task['timeout']});
+        let options = {fullPage: task['fullPage'], clip: task['clip']};
+
+        const image = sharp(await page.screenshot(options));
+
+        let buffer;
+
+        if (task['output']['type']=='jpg') {
+            let options = {quality: task['output']['quality'],
+                           progressive: task['output']['progressive']};
+            buffer = await image.jpeg(options).toBuffer();
+        }
+
+        fs.writeFile('test.jpg', buffer, 'binary', function(err){
             if (err) throw err;
             console.log('File saved.');
         });
