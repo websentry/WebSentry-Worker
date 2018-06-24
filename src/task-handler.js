bb4738ff75eecabf0d5a0d78e4353c557c0635af
CaviarChen <yzj@vlabpro.com>
2018-06-24 15:46:46 +1000

    update task-handler

--- src/task-handler.js
+++ src/task-handler.js
@@ -2,34 +2,43 @@ const fs = require('fs')
 const puppeteer = require('puppeteer');
 const sharp = require('sharp');
 
+taskCount = 0;
+
 function runTask(task) {
     (async () => {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
+        taskCount += 1;
+        try {
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
 
-        let viewport = task['viewport'];
-        if (viewport['height'] === undefined) viewport['height'] = 1024;
+            let viewport = task['viewport'];
+            if (viewport['height'] === undefined) viewport['height'] = 1024;
 
-        await page.setViewport(viewport);
-        await page.goto(task['url'], {timeout: task['timeout']});
-        let options = {fullPage: task['fullPage'], clip: task['clip']};
+            await page.setViewport(viewport);
+            await page.goto(task['url'], {timeout: task['timeout']});
+            let options = {fullPage: task['fullPage'], clip: task['clip']};
 
-        const image = sharp(await page.screenshot(options));
+            const image = sharp(await page.screenshot(options));
 
-        let buffer;
+            let buffer;
 
-        if (task['output']['type']=='jpg') {
-            let options = {quality: task['output']['quality'],
-                           progressive: task['output']['progressive']};
-            buffer = await image.jpeg(options).toBuffer();
+            if (task['output']['type']=='jpg') {
+                let options = {quality: task['output']['quality'],
+                               progressive: task['output']['progressive']};
+                buffer = await image.jpeg(options).toBuffer();
+            }
+
+            fs.writeFile('test.jpg', buffer, 'binary', function(err){
+                if (err) throw err;
+                console.log('File saved.');
+            });
+
+            await browser.close();
+        } catch (e) {
+            console.log(e);
+        } finally {
+            taskCount -= 1;
         }
-
-        fs.writeFile('test.jpg', buffer, 'binary', function(err){
-            if (err) throw err;
-            console.log('File saved.');
-        });
-
-        await browser.close();
     })();
 }
 
