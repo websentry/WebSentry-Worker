fa312bf1cf585887b385276849329fe6618d82fd
Zed (Zijun) Chen <c.zijunchen@gmail.com>
2020-02-17 20:26:49 +0800

    Update package && Add timeout to api request. (#10)
    * Allow setting a time to wait before taking screenshots.
    
    * Update package && Add timeout to api request.

--- src/api-client.ts
+++ src/api-client.ts
@@ -5,11 +5,11 @@ import {config} from "./config";
 
 
 async function init() {
-    return await sendApiRequest('init', {}, null);
+    return await sendApiRequest('init', {}, null, false);
 }
 
 async function fetchTask() {
-    return await sendApiRequest('fetch_task', {}, null);
+    return await sendApiRequest('fetch_task', {}, null, true);
 }
 
 async function submitTask(taskid, feedback, msg, image_buffer) {
@@ -22,7 +22,7 @@ async function submitTask(taskid, feedback, msg, image_buffer) {
     let retry = 0;
     while (true) {
         try {
-            let data = await sendApiRequest('submit_task', args, image_buffer);
+            let data = await sendApiRequest('submit_task', args, image_buffer, false);
             return data;
         } catch (error) {
             // TODO: log
@@ -34,8 +34,13 @@ async function submitTask(taskid, feedback, msg, image_buffer) {
     }
 }
 
-async function sendApiRequest(method, args, binary) {
+async function sendApiRequest(method, args, binary, is_long_polling) {
+    // Currently we don't handle the timeout case for long polling.
+    // We rely on the server side to close the request so the timeout for 
+    // long polling should be longer than the server side long polling.
+    let timeout = is_long_polling ? 120 * 1000 : 10 * 1000;
     let options : AxiosRequestConfig = {
+        timeout: timeout,
         url: config.apiServer + method,
         method: 'post',
         params: args,
